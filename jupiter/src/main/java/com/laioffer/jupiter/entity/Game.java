9870a6f4b7a1783f9fbbb30068e08f7fe2b283f1
williamliuzhenwei <zhl012@ucsd.edu>
2021-12-21 09:57:32 -0800

    update

---
+++ jupiter/src/main/java/com/laioffer/jupiter/entity/Game.java
@@ -0,0 +1,31 @@
+package com.laioffer.jupiter.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Game {
+
+    @JsonProperty("id")
+    private final String id;
+
+    @JsonProperty("name")
+    private final String name;
+
+    @JsonProperty("box_art_url")
+    private final String boxArtUrl;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBoxArtUrl() {
+        return boxArtUrl;
+    }
+
+
+
+}
+
--- jupiter/src/main/java/com/laioffer/jupiter/servlet/GameServlet.java
+++ jupiter/src/main/java/com/laioffer/jupiter/servlet/GameServlet.java
@@ -4,6 +4,9 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.jupiter.entity.Game;
 import org.json.JSONObject;
 import org.apache.commons.io.IOUtils;
 
@@ -11,14 +14,28 @@ import org.apache.commons.io.IOUtils;
 public class GameServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        response.setContentType("application/json");
+//        JSONObject game = new JSONObject();
+//        game.put("name", "World of Warcraft");
+//        game.put("developer", "Blizzard Entertainment");
+//        game.put("release_time", "Feb 11, 2005");
+//        game.put("website", "https://www.worldofwarcraft.com");
+//        game.put("price", 49.99);
+//        response.getWriter().print(game);
+
         response.setContentType("application/json");
-        JSONObject game = new JSONObject();
-        game.put("name", "World of Warcraft");
-        game.put("developer", "Blizzard Entertainment");
-        game.put("release_time", "Feb 11, 2005");
-        game.put("website", "https://www.worldofwarcraft.com");
-        game.put("price", 49.99);
-        response.getWriter().print(game);
+        ObjectMapper mapper = new ObjectMapper();
+        Game.Builder builder = new Game.Builder();
+        builder.setName("World of Warcraft");
+        builder.setDeveloper("Blizzard Entertainment");
+        builder.setReleaseTime("Feb 11, 2005");
+        builder.setWebsite("https://www.worldofwarcraft.com");
+        builder.setPrice(49.99);
+
+
+        Game game = builder.build();
+        response.getWriter().print(mapper.writeValueAsString(game));
+
     }
 
     @Override
